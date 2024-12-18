bae4adc3f18d5eaba98dbc3ab65dfdae2f65b5ba
SamoJanezic <janezic.orion@gmail.com>
2024-12-18 15:45:06 +0100

    update for future

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,34 +1,11 @@
-import { parser } from "./parseController.js";
-import { sqlInsert } from "../db/sql.js";
-import { db } from "../db/db.js";
+import { acordController } from "./controllers/acordController.js";
+import { avteraController } from "./controllers/avteraController.js";
+import { eventusController } from "./controllers/eventusController.js";
+import { colbyController } from "./controllers/colbyController.js";
+import { elkotexController } from "./controllers/elkotexController.js";
 
-export function insertIntoDb(obj) {
-	const data = parser(obj);
+const acord = new acordController();
+const avtera = new avteraController();
 
-	data.forEach((product) => {
-		let values = obj.keys.map((key) => {
-			if (key === "dobavitelj") {
-				return obj.name;
-			}
-			if (key === "niPodatka") {
-				return null;
-			}
-			if (product[key] === "") {
-				return null;
-			}
-			if (product[key] === undefined) {
-				return null;
-			}
-			if (product[key].constructor === Object) {
-				return Object.values(product[key])[0];
-			}
-			return product[key];
-		});
-
-		db.run(sqlInsert, values, (err) => {
-			if (err) {
-				console.error(err);
-			}
-		});
-	});
-}
+acord.executeAll();
+avtera.executeAll();
