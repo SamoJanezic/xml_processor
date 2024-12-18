8002d5d6b76399d7e09fabfd1e8eafff13eb2cf9
SamoJanezic <janezic.orion@gmail.com>
2024-12-18 10:02:02 +0100

    prettyfied

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,37 +1,34 @@
 import { parser } from "./parseController.js";
 import { sqlInsert } from "../db/sql.js";
-import { db } from "../db/db.js"
-
+import { db } from "../db/db.js";
 
 export function insertIntoDb(obj) {
+	const data = parser(obj);
 
-    const data = parser(obj);
+	data.forEach((product) => {
+		let values = obj.keys.map((key) => {
+			if (key === "dobavitelj") {
+				return obj.name;
+			}
+			if (key === "niPodatka") {
+				return null;
+			}
+			if (product[key] === "") {
+				return null;
+			}
+			if (product[key] === undefined) {
+				return null;
+			}
+			if (product[key].constructor === Object) {
+				return Object.values(product[key])[0];
+			}
+			return product[key];
+		});
 
-
-    data.forEach(product => {
-        let values = obj.keys.map(key => {
-            if (key === 'dobavitelj') {
-                return obj.name;
-            }
-            if (key === 'niPodatka') {
-                return null;
-            }
-            if (product[key] === '') {
-                return null;
-            }
-            if (product[key] === undefined) {
-                return null;
-            }
-            if (product[key].constructor === Object) {
-                return Object.values(product[key])[0];
-            }
-            return product[key];
-        });
-
-        db.run(sqlInsert, values , (err) => {
-            if(err){
-                console.error(err);
-            }
-        });
-    });
+		db.run(sqlInsert, values, (err) => {
+			if (err) {
+				console.error(err);
+			}
+		});
+	});
 }
