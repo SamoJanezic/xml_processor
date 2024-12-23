a08546efc9611f93219d459548557235b239ee4a
SamoJanezic <janezic.orion@gmail.com>
2024-12-23 08:37:08 +0100

    parser also returns attributes

--- controllers/parseController.js
+++ controllers/parseController.js
@@ -1,11 +1,14 @@
 import { XMLParser} from "fast-xml-parser";
 import { readFileSync } from 'fs';
 
-
-export function parser(file, nodes) {
-    const xmlFile = readFileSync(`${process.cwd()}/xml/${file}`, 'utf8');
-    const parser = new XMLParser();
+export function parser(file, nodes, encoding = 'utf8') {
+    const xmlFile = readFileSync(`${process.cwd()}/xml/${file}`, encoding);
+    const options = {
+        ignoreAttributes: false,
+        attributeNamePrefix : "@_"
+    };
+    const parser = new XMLParser(options);
     const json = parser.parse(xmlFile);
-    const products = eval(nodes);  //this is data in xml as json
-    return products;
+    const output = eval(nodes);
+    return output;
 }
