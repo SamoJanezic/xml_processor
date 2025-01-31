fe69d055e65f044c3b86cfeda755c8a6a1890364
SamoJanezic <janezic.orion@gmail.com>
2025-01-31 13:29:35 +0100

    added cors fix

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ const port = 3000;
 app.use(express.json());
 app.use((req, res, next) => {
     allow(res);
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
     next();
 });
 
