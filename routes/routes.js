d8e171ae84eea45d8affc97ce5f3b28abb245c5c
SamoJanezic <janezic.orion@gmail.com>
2025-01-31 13:31:10 +0100

    deleted unnecessary code

--- routes/routes.js
+++ routes/routes.js
@@ -1,13 +1,9 @@
 import express from "express";
-import { selectAll, findOne } from "../db/sql.js";
+import { selectAll, findOne, updateItem } from "../db/sql.js";
 import { formatAndUpdate, readId} from "../middlewares/baseMiddleware.js";
 
 const router = express.Router();
 
-router.post("/post", (req, res) => {
-	console.log(req.body)
-	res.send("POST request to the homepage");
-});
 
 router.get("/getData", async (req, res) => {
 	const columns = ['id', 'ean', 'kategorija', 'izdelek_ime', 'cena_nabavna', 'dealer_cena', 'ppc', 'balgovna_znamka', 'dobavitelj'];
@@ -15,21 +11,19 @@ router.get("/getData", async (req, res) => {
     res.status(200).json(getData);
 });
 
-router.delete("/delete", (req, res) => {
-	console.log(req.body);
-	res.status(200);
-});
-
-router.put("/put", (req, res) => {
-	res.status(200);
-	formatAndUpdate(req.body);
-	res.end("Data received");
-});
-
 router.get("/getSingle", async (req, res) => {
 	const id = readId(req.query);
 	const singleData = await findOne('id', id);
 	res.status(200).json(singleData);
 });
+
+router.put("/update", (req, res) => {
+	res.status(200);
+	// console.log(req.body.values);
+	const data = formatAndUpdate(req.body.values);
+	updateItem(data.id, data.arr);
+	res.end("Data received");
+});
+
 
 export default router;
