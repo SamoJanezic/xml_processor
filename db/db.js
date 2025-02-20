84a65c6bfe985e848ba1170e172fa334e8b7b71f
SamoJanezic <janezic.orion@gmail.com>
2025-02-20 15:41:42 +0100

    implemented sequelize

--- db/db.js
+++ db/db.js
@@ -3,3 +3,14 @@ import sqlite from "sqlite3";
 export const db = new sqlite.Database(`${process.cwd()}/db/sqlite/test.db`, sqlite.OPEN_READWRITE, (err) => {
     if(err)console.error(err);
 });
+
+// import { Sequelize, DataTypes } from "sequelize";
+
+// export const db = new Sequelize({
+// 	dialect: "sqlite",
+// 	storage: `${process.cwd()}/db/sqlite/test.db`,
+// 	define: {
+// 		freezeTableName: true,
+// 		timestamps: false,
+// 	},
+// });
