761ba7a5111e83f5ac977ea08eaaf54e81547b16
SamoJanezic <janezic.orion@gmail.com>
2025-02-27 15:56:18 +0100

    creates all neccessary tables

--- scripts/up.js
+++ scripts/up.js
@@ -1,4 +1,15 @@
 import { createTable } from "../db/sql.js";
 import { izdelekTest } from "../Models/test.js";
+import { Izdelek } from "../Models/Izdelek.js";
+import { IzdelekDobavitelj } from "../Models/IzdelekDobavitelj.js";
+import { Kategorija } from "../Models/Kategorija.js";
+import { DobaviteljTabela } from "../Models/Dobavitelj.js"
+import { Komponenta } from "../Models/Komponenta.js";
 
 createTable(izdelekTest);
+createTable(Izdelek);
+createTable(IzdelekDobavitelj);
+createTable(Kategorija);
+createTable(izdelekTest);
+createTable(DobaviteljTabela);
+createTable(Komponenta);
