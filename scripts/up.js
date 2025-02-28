d64b17b8183ea9e7bb1496f2a7c335a9f800bdef
SamoJanezic <janezic.orion@gmail.com>
2025-02-28 16:00:05 +0100

    added atribut to import list

--- scripts/up.js
+++ scripts/up.js
@@ -5,11 +5,13 @@ import { IzdelekDobavitelj } from "../Models/IzdelekDobavitelj.js";
 import { Kategorija } from "../Models/Kategorija.js";
 import { DobaviteljTabela } from "../Models/Dobavitelj.js"
 import { Komponenta } from "../Models/Komponenta.js";
+import { Atribut } from "../Models/Atribut.js";
 
-createTable(izdelekTest);
+// createTable(izdelekTest);
 createTable(Izdelek);
 createTable(IzdelekDobavitelj);
 createTable(Kategorija);
 createTable(izdelekTest);
 createTable(DobaviteljTabela);
 createTable(Komponenta);
+createTable(Atribut);
