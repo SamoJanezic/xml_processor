86303645a46ac8b92b59f035d8da0411b867d8cc
SamoJanezic <janezic.orion@gmail.com>
2025-03-17 15:51:49 +0100

    fixed unique indexes

--- Models/Atribut.js
+++ Models/Atribut.js
@@ -2,39 +2,39 @@ import { db } from "../db/db.js";
 import { DataTypes } from "sequelize";
 
 export const Atribut = db.define(
-    "ATRIBUT",
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-            autoIncrement: true,
-        },
-        izdelek_ean: {
-            type: DataTypes.STRING,
-            references: {
-                model: "IZDELEK",
-                key: "ean",
-            },
-            allowNull: false
-        },
-        KOMPONENTA_komponenta: {
-            type:DataTypes.STRING,
-            references: {
-                model: "KOMPONENTA",
-                key: "komponenta"
-            }
-        },
-        atribut: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-    },
-    {
-        indexes: [
-            {
-                unique:true,
-                fields: ['atribut', 'KOMPONENTA_komponenta']
-            }
-        ]
-    }
+	"ATRIBUT",
+	{
+		id: {
+			type: DataTypes.INTEGER,
+			primaryKey: true,
+			autoIncrement: true,
+		},
+		izdelek_ean: {
+			type: DataTypes.STRING,
+			references: {
+				model: "IZDELEK",
+				key: "ean",
+			},
+			allowNull: false,
+		},
+		KOMPONENTA_komponenta: {
+			type: DataTypes.STRING,
+			references: {
+				model: "KOMPONENTA",
+				key: "komponenta",
+			},
+		},
+		atribut: {
+			type: DataTypes.STRING,
+			allowNull: true,
+		},
+	},
+	{
+		indexes: [
+			{
+				unique: true,
+				fields: ["izdelek_ean", "atribut", "KOMPONENTA_komponenta"],
+			},
+		],
+	}
 );
