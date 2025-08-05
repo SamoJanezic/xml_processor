819c9a87a9b8d822fd39effb75379d6e4029953b
SamoJanezic <janezic.orion@gmail.com>
2025-08-05 11:09:11 +0200

    added more xmls

--- app.js
+++ app.js
@@ -5,26 +5,38 @@ import { elkotexController } from "./controllers/elkotexController.js";
 import { eventusController } from "./controllers/eventusController.js";
 import { asbisController } from "./controllers/asbisController.js";
 import { alsoController } from "./controllers/alsoController.js";
+import { liebherrController } from "./controllers/liebherrController.js";
+import { gorenjeController } from "./controllers/gorenjeController.js";
+import { bshController } from "./controllers/bshController.js";
 
-export function executeXML() {
-	const also = new alsoController();
-	also.executeAll();
+// export function executeAll() {
+	// const also = new alsoController();
+	// also.executeAll();
 
-	const acord = new acordController();
-	acord.executeAll();
+	// const acord = new acordController();
+	// acord.executeAll();
 
-	const avtera = new avteraController();
-	avtera.executeAll();
+	// const avtera = new avteraController();
+	// avtera.executeAll();
 
-	const eventus = new eventusController();
-	eventus.executeAll();
+	// const eventus = new eventusController();
+	// eventus.executeAll();
 
-	const asbis = new asbisController();
-	asbis.executeAll();
+	// const asbis = new asbisController();
+	// asbis.executeAll();
 
-	const colby = new colbyController();
-	colby.executeAll();
+	// const elkotex = new elkotexController();
+	// elkotex.executeAll();
 
-	const elkotex = new elkotexController();
-	elkotex.executeAll();
-}
+	// const colby = new colbyController();
+	// colby.executeAll();
+
+	// const liebherr = new liebherrController();
+	// liebherr.executeAll();
+
+	// const gorenje = new gorenjeController();
+	// gorenje.executeAll();
+
+	const bsh = new bshController();
+	bsh.executeAll();
+// }
