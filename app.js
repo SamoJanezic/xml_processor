7ffc07b8f0701e1615dfb3395faddf4685ce6689
SamoJanezic <janezic.orion@gmail.com>
2025-08-08 15:53:17 +0200

    refactor of app.js and addition of DI

--- app.js
+++ app.js
@@ -1,42 +1,12 @@
-import { acordController } from "./controllers/acordController.js";
-import { avteraController } from "./controllers/avteraController.js";
-import { colbyController } from "./controllers/colbyController.js";
-import { elkotexController } from "./controllers/elkotexController.js";
-import { eventusController } from "./controllers/eventusController.js";
-import { asbisController } from "./controllers/asbisController.js";
-import { alsoController } from "./controllers/alsoController.js";
-import { liebherrController } from "./controllers/liebherrController.js";
-import { gorenjeController } from "./controllers/gorenjeController.js";
-import { bshController } from "./controllers/bshController.js";
+import { controllerMap } from "./controllers/index.js";
+import { categoryMap } from "./controllers/categoryMaps/index.js";
 
-// export function executeAll() {
-	// const also = new alsoController();
-	// also.executeAll();
+export function executeAll() {
+	Object.entries(controllerMap).forEach(([key, Controller]) => {
+		const category = categoryMap[key];
+		const instance = new Controller(category);
+		instance.executeAll();
+	});
+}
 
-	// const acord = new acordController();
-	// acord.executeAll();
-
-	// const avtera = new avteraController();
-	// avtera.executeAll();
-
-	// const eventus = new eventusController();
-	// eventus.executeAll();
-
-	// const asbis = new asbisController();
-	// asbis.executeAll();
-
-	// const elkotex = new elkotexController();
-	// elkotex.executeAll();
-
-	// const colby = new colbyController();
-	// colby.executeAll();
-
-	// const liebherr = new liebherrController();
-	// liebherr.executeAll();
-
-	// const gorenje = new gorenjeController();
-	// gorenje.executeAll();
-
-	const bsh = new bshController();
-	bsh.executeAll();
-// }
+executeAll();
---
+++ controllers/index.js
@@ -0,0 +1,23 @@
+import { AcordController } from "./AcordController.js";
+import { AvteraController } from "./AvteraController.js";
+import { ColbyController } from "./ColbyController.js";
+import { ElkotexController } from "./ElkotexController.js";
+import { EventusController } from "./EventusController.js";
+import { AsbisController } from "./AsbisController.js";
+import { AlsoController } from "./AlsoController.js";
+import { LiebherrController } from "./LiebherrController.js";
+import { GorenjeController } from "./GorenjeController.js";
+import { BshController } from "./BshController.js";
+
+export const controllerMap = {
+  // also: AlsoController,
+  // acord: AcordController,
+  // avtera: AvteraController,
+  elkotex: ElkotexController,
+  // eventus: EventusController,
+  // asbis: AsbisController,
+  // colby: ColbyController,
+  // liebherr: LiebherrController,
+  // gorenje: GorenjeController,
+  // bsh: BshController,
+};
