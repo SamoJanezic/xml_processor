375beab4f8d49fb46db99e62377bd9e9eaeb7927
SamoJanezic <janezic.orion@gmail.com>
2025-08-08 15:54:28 +0200

    addition of category map

---
+++ controllers/categoryMaps/index.js
@@ -0,0 +1,23 @@
+import acordCategoryMap from "./acordCategory.js";
+import eventusCategoryMap from "./eventusCategory.js";
+import alsoCategoryMap from "./alsoCategory.js";
+import avteraCategoryMap from "./avteraCategory.js";
+import asbisCategoryMap from "./asbisCategory.js";
+import elkotexCategoryMap from "./elkotexCategory.js";
+// import colbyCategoryMap from "./colbyCategory.js";
+// import liebherrCategoryMap from "./liebherrCategory.js";
+// import gorenjeCategoryMap from "./gorenjeCategory.js";
+import bshCategoryMap from "./bshCategory.js";
+
+export const categoryMap = {
+  also: alsoCategoryMap,
+  acord: acordCategoryMap,
+  avtera: avteraCategoryMap,
+  eventus: eventusCategoryMap,
+  asbis: asbisCategoryMap,
+  elkotex: elkotexCategoryMap,
+  // colby: colbyCategoryMap,
+  // liebherr: liebherrCategoryMap,
+  // gorenje: gorenjeCategoryMap,
+  bsh: bshCategoryMap,
+};
