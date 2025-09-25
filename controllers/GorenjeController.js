0a52bd0974de38bc0736b451912e7708e2d0f638
SamoJanezic <janezic.orion@gmail.com>
2025-09-25 10:34:32 +0200

    updated controller

--- controllers/GorenjeController.js
+++ controllers/GorenjeController.js
@@ -1,52 +1,59 @@
-import { parser } from "./parseController.js";
+import { xmlParser } from "./parseController.js";
 import DobaviteljController from "./DobaviteljController.js";
 
 export class GorenjeController extends DobaviteljController {
 	name = "gorenje";
-	nodes = "mabagor.products";
+	nodes = "mabagor";
 	file = "gorenje.xml";
 	encoding = "utf8";
 	keys = [
-		"EAN",
-		"izdelekIme",
-		"niPodatka",
+		"ean",
+		"izdelek_ime",
+		"kratki_opis",
 		"opis",
-		"nabavnaCena",
-		"DC",
-		"PPC",
-		"davcnaStopnja",
-		"slikaMala",
-		"slikaVelika",
-		"dodatneSlike",
-		"dodatneLastnosti",
-		"blagovnaZnamka",
+		"cena_nabavna",
+		"dealer_cena",
+		"ppc",
+		"davcna_stopnja",
+		"slika_mala",
+		"slika_velika",
+		"dodatne_slike",
+		"dodatne_lastnosti",
+		"blagovna_znamka",
+		"kategorija",
+		"eprel_id",
+		"zaloga",
 	];
 
 
 	createDataObject() {
-		const data = parser(this.file, this.nodes, this.encoding);
+		const data = xmlParser(this.file, this.nodes, this.encoding);
 		// data.forEach(el => {
 		// 	console.log(el)
 		// })
-		data.product.flatMap((el) => console.log(el))
+		console.log(data.features.feature)
+		// data.products.product.flatMap((el) => console.log(el));
 		// this.getSingleData(data[0]);
 	}
 
 	getSingleData(data) {
 		const singleObject = {
-			EAN: data.productContent.product_eans,
-			izdelekIme: data.izdelekIme,
-			niPodatka: data.niPodatka,
-			opis: data.opis,
-			nabavnaCena: data.nabavnaCena,
-			DC: data.DC,
-			PPC: data.PPC,
-			davcnaStopnja: data.davcnaStopnja,
-			slikaMala: data.slikaMala,
-			slikaVelika: data.slikaVelika,
-			dodatneSlike: this.splitSlike(data.dodatneSlike),
-			dodatneLastnosti: this.splitDodatneLastnosti(data.dodatneLastnosti),
-			blagovnaZnamka: data.blagovnaZnamka
+			ean: data.product_content.basic_information.product_eans.ean,
+			izdelek_ime: data.product_content.basic_information.product_titles.title,
+			kratki_opis: data.product_content.basic_information.product_descriptions.short_description,
+			opis: data.product_content.basic_information.product_descriptions.long_description,
+			cena_nabavna: data.product_content.basic_information.product_prices.msrp,
+			dealer_cena: null,
+			ppc: data.product_content.basic_information.product_prices.msdp,
+			davcna_stopnja: '22',
+			// slika_mala:
+			// slika_velika:
+			// dodatne_slike:
+			// dodatne_lastnosti:
+			blagovna_znamka: data.product_category_hierarchy.child_category,  //where category_context = product_brand
+			kategorija: data.product_category_hierarchy.child_category, // where category_context = product_type,
+			eprel_id: null,
+			zaloga: 'preveriti',
 		};
 		console.log(singleObject)
 	}
@@ -56,6 +63,7 @@ export class GorenjeController extends DobaviteljController {
 	}
 
 	sortCategories() {
+		
 	}
 
 	parseObject(obj) {}
@@ -64,7 +72,9 @@ export class GorenjeController extends DobaviteljController {
 		// console.log("🚀 ~ gorenjeController ~ splitDodatneLastnosti ~ allData:", this.allData)
 	}
 
-	splitSlike() {}
+	splitSlike() {
+
+	}
 
 	executeAll() {
 		this.createDataObject();
